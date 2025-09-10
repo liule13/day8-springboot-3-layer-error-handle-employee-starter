16779cd4fa7143ef8e369c0a73151de2a02a73a4
liule13 <LEO.LIU@OOCL.COM>
2025-09-10 11:47:59 +0800

    refactor: update all controller to service and repository

---
+++ src/main/java/com/example/demo/respository/EmployeeRepository.java
@@ -0,0 +1,80 @@
+package com.example.demo.respository;
+
+import com.example.demo.entity.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+@Repository
+public class EmployeeRepository {
+    private final List<Employee> employees = new ArrayList<>();
+
+    //getEmployees
+    public List<Employee> getEmployees(String gender, Integer page, Integer size) {
+        Stream<Employee> stream = employees.stream();
+        if (gender != null) {
+            stream = stream.filter(employee -> employee.getGender().compareToIgnoreCase(gender) == 0);
+        }
+        if (page != null && size != null) {
+            stream = stream.skip((long) (page - 1) * size).limit(size);
+        }
+        return stream.toList();
+    }
+
+    public Employee getEmployeeById(int id) {
+        return employees.stream()
+                .filter(employee -> employee.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Employee createEmployee(Employee employee) {
+        employee.setId(employees.size() + 1);
+        employees.add(employee);
+        return employee;
+    }
+
+    public Employee updateEmployee(int id, Employee updatedEmployee) {
+        Employee found = null;
+        for (Employee e : employees) {
+            if (Objects.equals(e.getId(), id)) {
+                found = e;
+                break;
+            }
+        }
+        if (found == null) {
+            return  found;
+        }
+        found.setName(updatedEmployee.getName());
+        found.setAge(updatedEmployee.getAge());
+        found.setGender(updatedEmployee.getGender());
+        found.setSalary(updatedEmployee.getSalary());
+        return found;
+    }
+
+    public void deleteEmployee(int id) {
+        Employee found = null;
+        for (Employee e : employees) {
+            if (e.getId() == id) {
+                found = e;
+                break;
+            }
+        }
+        if (found == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found with id: " + id);
+        }
+        employees.remove(found);
+    }
+
+    public void clear() {
+        employees.clear();
+    }
+}

7287850780dd9497af68aef6d553f08088bcea6b
liule13 <LEO.LIU@OOCL.COM>
2025-09-11 09:21:44 +0800

    refactor: delete the unused code

--- src/main/java/com/example/demo/controller/CompanyController.java
+++ src/main/java/com/example/demo/controller/CompanyController.java
@@ -5,13 +5,11 @@ import com.example.demo.service.CompanyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    private final List<Company> companies = new ArrayList<>();
     private final CompanyService companyService;
 
     public CompanyController(CompanyService companyService) {
